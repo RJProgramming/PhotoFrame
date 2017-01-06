34bcbbfdf26f0330f4de6a7c4f66a12b8fc442d1
Robert Perez <Robert@roberts-mbp.simpsonmfg.com>
2017-01-06 10:02:49 -0800

    Initial Commit

---
+++ Lil Memer/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Lil Memer
+//
+//  Created by Robert Perez on 1/6/17.
+//  Copyright © 2017 Robert Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var imageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
+        imageView.image = UIImage(named: "choose")
+        imageView.isUserInteractionEnabled = true
+        imageView.contentMode = .scaleAspectFit
+        
+        scrollView.addSubview(imageView)
+        
+        let tapGestureRecongnizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.loadImage))
+        tapGestureRecongnizer.numberOfTapsRequired = 1
+        imageView.addGestureRecognizer(tapGestureRecongnizer)
+    }
+    
+    func loadImage(recognizer: UITapGestureRecognizer){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        imageView.contentMode = UIViewContentMode.center
+        imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        
+        scrollView.contentSize = image.size
+        
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleHeight, scaleWidth)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = 3
+        scrollView.zoomScale = minScale
+        
+        centerScrollViewContents()
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func centerScrollViewContents(){
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width{
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2
+        }else{
+            contentsFrame.origin.x = 0
+        }
+    
+        if contentsFrame.size.height < boundsSize.height{
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2
+        }else{
+            contentsFrame.origin.y = 0
+        }
+
+        imageView.frame = contentsFrame
+
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+
+    @IBAction func save(_ sender: Any) {
+        UIGraphicsBeginImageContextWithOptions(scrollView.bounds.size, true, UIScreen.main.scale)
+        let offset = scrollView.contentOffset
+        
+        UIGraphicsGetCurrentContext()!.translateBy(x: -offset.x, y: -offset.y)
+        scrollView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Image Saved", message: "your image has been saved", preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+        alert.addAction(UIAlertAction(title: "neat", style: .default, handler: nil))
+        
+    }
+
+}
+
